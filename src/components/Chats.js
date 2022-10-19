2f45c4e6829e576a10ac2f8ee280a922371c3637
Shaurya-dub <shauryadubey@hotmail.ca>
2022-10-19 12:08:00 -0400

    built chats component

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import {ChatEngine} from 'react-chat-engine';
+import {auth} from '../firebase'
 const Chats = () => {
+    const history = useHistory();
+    const handleLogout = async () => {
+        await auth.signOut();
+        history.pushState('/')
+    }
     return (
-        <h1>Chats</h1>
+        <div className="chats-page">
+            <div className="nav-bar">
+                <div className="logo-tab">
+                    Unichat
+                </div>
+                <div onClick={handleLogout} className="logout-tab">
+                    Logout
+                </div>
+            </div>
+            <ChatEngine 
+            height='calc(100vh - 66px)'
+            projectId='*****'
+            userName = '.'
+            userSecret='.'
+            />
+        </div>
     )
 }
 
