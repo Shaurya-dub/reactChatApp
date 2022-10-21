5c1df435493c93b0a656c05f497104b346fce40d
Shaurya-dub <shauryadubey@hotmail.ca>
2022-10-21 12:15:18 -0400

    trying to establish color scheme in custom chat component

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -69,14 +69,15 @@ const Chats = () => {
   return (
     <div className="chats-page">
       <div className="nav-bar">
-        <div className="logo-tab">Unichat</div>
+        <div className="logo-tab">React Chat</div>
         <div onClick={handleLogout} className="logout-tab">
           Logout
         </div>
       </div>
       <ChatEngine
         height="calc(100vh - 66px)"
-        projectID= {process.env.REACT_APP_CHAT_ENGINE_ID}
+        background-color="blue"
+        projectID={process.env.REACT_APP_CHAT_ENGINE_ID}
         userName={user.email}
         userSecret={user.uid}
       />
